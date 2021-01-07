0b481b1ab0710febabf7d45c6f29a3d41d35ecff
Ygor Riam <ygoriam@hotmail.com>
2021-01-07 18:57:34 -0300

    Add routes and StepsHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes';
+
 
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Home/>
-    </>
-  );
+    
+    <Routes/>
+   
+  )
 }
 
 export default App;
