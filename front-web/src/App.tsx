556d5aac8484275cd574b213e76b13ce88cc636e
Ygor Riam <ygoriam@hotmail.com>
2021-01-08 19:06:43 -0300

    finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,3 +1,6 @@
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routes from './Routes';
 
@@ -5,9 +8,10 @@ import Routes from './Routes';
 
 function App() {
   return (
-    
+    <>
     <Routes/>
-   
+    <ToastContainer/>
+   </>
   )
 }
 
